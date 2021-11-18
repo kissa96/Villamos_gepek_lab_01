87da495caa73c980b6631984abd86fbe2598ee74
kissattila96@gmail.com <kissattila96@gmail.com>
2021-11-18 22:29:06 +0100

    Script update

--- README.md
+++ README.md
@@ -9,8 +9,5 @@ Hogyan ellenőrizheted le, hogy működik minden?
 1. legyen fent a gépeden g++ (c++ fordító) valamint python interpreter
 2. töltsd le a fájlokat GitHub-ról (a mappák hierarchiája maradjon úgy, ahogy van)
 3. navigálj oda, ahová letöltötted a forráskód mappát
-4. command line (cmd) c++ fordításhoz és exe generáláshoz írd be: g++ vg_res_calc.cpp -o futtass_engem.exe
-5. futtasd le a programot
-6. kapsz egy resistance_values.txt állományt
-7. command line: py main.py 
-8. megkapod a diagramot
+4. command line: py main.py
+5. A többit intézi a script
--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import os
 
-with open('resistance_values.txt') as f:
-    lines = f.readlines()
-    for i in range(3):
-        lines.pop(0)
-    x = [sbstr.strip().split('\t')[0] for sbstr in lines]
-    y = [round(float(sbstr.strip().split('\t')[1]), 4) for sbstr in lines]
-    plt.title('Coil resistance ')
-    plt.xlabel('Temperature[C]')
-    plt.ylabel('Resistance[ohms]')
-    plt.xticks(rotation=90)
-    plt.plot(x, y)
-    plt.show()
+os.system("del resistance_values.txt")
+os.system("g++ vg_res_calc.cpp -o temp_run.exe")
+os.system("temp_run.exe")
+os.system("py diagram.py")
+os.system("del temp_run.exe")
